a646dca8ab94cec7748e174bd2d44bfc7167e203
Imade-Eddine-Ouazane <imadeddine.ouazane@gmail.com>
2026-01-11 20:07:52 +0100

    add all project files

---
+++ .angular/cache/19.2.19/ecom-app-frontend/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,41 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell
+} from "./chunk-W6NFEZSG.js";
+import "./chunk-FWTTGBFI.js";
+import "./chunk-J636S72Q.js";
+import "./chunk-GSGOTBGJ.js";
+import "./chunk-UTJ7PBUX.js";
+import "./chunk-IPEV7CEU.js";
+import "./chunk-UB7WHBGV.js";
+import "./chunk-LYW5XXXZ.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  withAppShell,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
